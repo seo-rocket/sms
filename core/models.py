66c5c67d62f78222fe042d67d846b5dab431b5ca
seo-rocket <admin@seo-rocket.ru>
2025-03-18 22:37:11 +0400

    fix

--- core/models.py
+++ core/models.py
@@ -8,10 +8,11 @@ class Service(models.Model):
         return self.name
 
 class SMS(models.Model):
-    phone_number = models.CharField(max_length=20, unique=True)
+    phone_number = models.CharField(max_length=20)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     message = models.TextField()
     received_at = models.DateTimeField(auto_now_add=True)
+    region = models.CharField(max_length=20)
 
     def __str__(self):
         return f"SMS from {self.service} to {self.phone_number}"

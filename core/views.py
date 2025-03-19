2e41b9e85db2915e5a800a0d9718b8dc3c2fa52d
Metra 758 <golovanovarcenij49@gmail.com>
2025-03-19 14:30:36 +0400

    add sms list

--- core/views.py
+++ core/views.py
@@ -7,6 +7,13 @@ from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from .models import *
 from .serializers import *
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.shortcuts import render
+from django.db.models import Count
+
+
+
 
 
 
@@ -37,3 +44,40 @@ class SMSViewSet(viewsets.ModelViewSet):
 class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+
+
+
+
+
+def index(request):
+    # Получаем фильтр по дате, если он указан
+    date_filter = request.GET.get('date_filter', None)
+    
+    # Фильтрация сообщений по дате, если выбран фильтр
+    if date_filter:
+        sms_list = SMS.objects.filter(received_at__date=date_filter)
+    else:
+        sms_list = SMS.objects.all()
+
+    # Общая статистика
+    total_sms = sms_list.count()
+    total_services = Service.objects.count()
+    total_accounts = Account.objects.count()
+
+    # Количество SMS по регионам
+    region_counts = SMS.objects.values('region').annotate(count=Count('region'))
+
+    # Статистика по сервисам
+    service_counts = Service.objects.annotate(count=Count('sms'))
+
+    context = {
+        'sms_list': sms_list,
+        'total_sms': total_sms,
+        'total_services': total_services,
+        'total_accounts': total_accounts,
+        'region_counts': region_counts,
+        'service_counts': service_counts,
+        'date_filter': date_filter
+    }
+
+    return render(request, 'index.html', context)
